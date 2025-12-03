4b85cd4d779cbff9bc2781c14c07075d85d6c3eb
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-03 14:39:41 +0800

    add multiple pages

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const NotFound:React.FC = () => {
+  return (
+    <div>
+        <h2>Page Not Found</h2>
+        <p>The page that you are looking for cannot be found at the moment</p>
+    </div>
+  )
+}
+
+export default NotFound;
